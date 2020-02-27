f94bd6fc5534a6c256853e09e37620f107dcf4a9
Hyejin Kim <cshjkim@gmail.com>
2020-02-26 17:56:23 -0800

    draft of basic folders and fils

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// add exports 
+
+module.exports = {
+  
+};
---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const  = require("");
+
+// Book routes
+router.use();
+
+module.exports = router;
