46db629439a9938b1dd6f9d85397a4301ee760b8
Hyejin Kim <cshjkim@gmail.com>
2020-02-28 21:33:57 -0800

    touch some css to make it stylish

--- src/components/NavTabs/NavTabs.js
+++ src/components/NavTabs/NavTabs.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./NavTabs.css"
 
 function NavTabs() {
   return (
 
-    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
+    <nav className="navbar navbar-expand-lg">
       <a className="navbar-brand" href="/">
         CLICKY GAME START!
       </a>
+      Click Image to Begin!
     </nav>
   );
 }
